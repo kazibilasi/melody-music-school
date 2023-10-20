b9f24e6e5616bc50dcb222c332296c1d9334f2b3
kazibilsi <kazibilasi@gmail.com>
2023-10-20 10:16:17 +0600

    error solve

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -3,8 +3,9 @@ import Header from '../Header/Header';
 import PrivateLesson from '../PrivateLesson/PrivateLesson';
 import SummerArtsGarden from '../Body/SummerArtsGarden'
 import PopularClasses from '../Body/PopularClasses/PopularClasses';
-import ExtraSection from '../Body/extraSection';
+
 import TopInstructors from '../TopInstructors/TopInstructors';
+import ExtraSection from '../Body/ExtraSection';
 
 const Home = () => {
     return (
@@ -13,7 +14,7 @@ const Home = () => {
 
             <SummerArtsGarden></SummerArtsGarden>
             <PopularClasses></PopularClasses>
-            <ExtraSection></ExtraSection>
+           <ExtraSection></ExtraSection>
             <PrivateLesson></PrivateLesson>
             <TopInstructors></TopInstructors>
         </div>
